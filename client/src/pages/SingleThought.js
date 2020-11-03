2f3baf2b9f528a9e7aa01ab92111769dc6431af7
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-03 14:29:11 -0600

    reactions, profile and friends added

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
 
@@ -31,6 +32,7 @@ if(loading) {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions}/>}
     </div>
   );
 };
