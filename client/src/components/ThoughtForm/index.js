a912e602f16fef1e9a6605246d573ecae3c35f00
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-05 13:36:38 -0600

    add thought mutation added to homepage and profile

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 
 
@@ -18,14 +20,48 @@ const handleChange = event => {
 
   const handleFormSubmit = async event => {
       event.preventDefault();
-      setText(''); 
-      setCharacterCount(0);
+      try {
+        // add thought to database
+        await addThought({
+          variables: { thoughtText }
+        });
+    
+        // clear form value
+        setText('');
+        setCharacterCount(0);
+      } catch (e) {
+        console.error(e);
+      }
   };
+
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+      update(cache, {data: {addThought} }) {
+        try{
+          //read what's currently in the cache
+          const{ thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+          //prepare the newest thought to the front of the array
+          cache.writeQuery({
+              query: QUERY_THOUGHTS,
+              data: {thoughts: [addThought, ...thoughts]}
+          });
+     } catch (e) {
+         console.log(e);
+     }
+     //update me object's cache, appending new thought to the end of the array
+     const { me } = cache.readQuery({ query: QUERY_ME });
+     cache.writeQuery({
+         query: QUERY_ME,
+         data: {me: {...me, thoughts: [me.thoughts, addThought]}}
+     });
+    }
+  });
    
 return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
-       Character Count: {characterCount}/280
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch"
         onSubmit={handleFormSubmit}
